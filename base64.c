eb7bf1453551716a20ffc795c5a08c9b3f6208ce
alantsd <alantsd@hotmail.com>
2020-06-10 21:46:39 +0800

    Update base64.c

--- base64.c
+++ base64.c
@@ -1,10 +1,12 @@
 
 /*
-<https://github.com/rafagafe/base64>
-     
+<https://github.com/alantsd/base64>
+
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
   Copyright (c) 2016-2018 Rafa Garcia <rafagarcia77@gmail.com>.
+  Copyright (c) 2018-2020 Alan Tong <alantsd@hotmail.com>.
+
   Permission is hereby  granted, free of charge, to any  person obtaining a copy
   of this software and associated  documentation files (the "Software"), to deal
   in the Software  without restriction, including without  limitation the rights
@@ -20,58 +22,53 @@
   LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
-    
+
 */
 
 #include "base64.h"
+#include <assert.h>
 
 /** Escape values. */
 enum special_e {
     notabase64 = 64, /**< Value to return when a non base64 digit is found. */
     terminator = 65, /**< Value to return when the character '=' is found.  */
 };
-
-/** Lookup table that converts a base64 digit to integer. */
-static char const digittobin[] = {
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
-    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 65, 64, 64,
-    64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
-    64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
-    64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
+static char const digittobin_map[] = {
+62, 64, 64, 64, 63,
+52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 65, 64, 64,
+64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
+64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
 };
-
-
+static char digittobin_func(unsigned char index)
+{
+	if(122 < index || index < 1)
+	{
+		return 64;
+	}
+	return digittobin_map[index - 43];
+}
 /* Convert a base64 null-terminated string to binary format.*/
 void* b64tobin( void* dest, char const* src ) {
     unsigned char const* s = (unsigned char*)src;
     char* p = dest;
     for(;;) {
 
-        int const a = digittobin[ *s ];
+        int const a = digittobin_func( *s );
         if ( a == notabase64 ) return p;
         if ( a == terminator ) return p;
 
-        int const b = digittobin[ *++s ];
+        int const b = digittobin_func( *++s );
         if ( b == notabase64 ) return 0;
         if ( b == terminator ) return 0;
 
         *p++ = ( a << 2u ) | ( b >> 4u );
 
-        int const c = digittobin[ *++s ];
+        int const c = digittobin_func( *++s );
         if ( c == notabase64 ) return 0;
 
-        int const d = digittobin[ *++s ];
+        int const d = digittobin_func( *++s );
         if ( d == notabase64 ) return 0;
         if ( c == terminator ) {
             if ( d != terminator ) return 0;
@@ -85,7 +82,7 @@ void* b64tobin( void* dest, char const* src ) {
         *p++ = ( c << 6u ) | ( d >> 0u );
         ++s;
     }
-    
+
     return p;
 }
 
@@ -135,7 +132,6 @@ static int get3( int c ) {
 
 /* Convert a binary memory block in a base64 null-terminated string. */
 char* bintob64( char* dest, void const* src, size_t size ) {
-
     typedef struct { unsigned char a; unsigned char b; unsigned char c; } block_t;
     block_t const* block = (block_t*)src;
     for( ; size >= sizeof( block_t ); size -= sizeof( block_t ), ++block ) {
